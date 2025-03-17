7876a93324c70157d2c3d41cbbcfa01397a4a6e9
kerccv <kerakk3113@gmail.com>
2025-03-17 21:35:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -81,5 +81,5 @@ app.post("/apply-key", (req, res) => {
 
 
 // Запуск сервера
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Сервер работает на порту ${PORT}`));
